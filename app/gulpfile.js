4491bf97146014a3f491a0710db05b0276a7b543
Rafael4001 <rafal.ciesielczuk@gmail.com>
2017-08-09 12:22:35 +0200

    i'm still cleaning

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,5 +1,14 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+    sass = require('gulp-sass');
 
-gulp.task('hello', function() {
+
+gulp.task('hello', function () {
     console.log('Hello Zell');
+});
+
+
+gulp.task('sass', function(){
+    return gulp.src('app/scss/styleSCSS.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('app/css'))
 });
